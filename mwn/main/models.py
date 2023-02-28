6e4157e4f20d441bf4f6a935eac2e2af8b8d3fad
KianRahonam <manohar.naik@outlook.com>
2023-02-28 14:53:26 +0530

    'all'

---
+++ mwn/main/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class UserManagement(models.Model):
+
+    firstName = models.CharField(max_length=40)
+    lastName = models.CharField(max_length=40)
+    emailId = models.EmailField(max_length=100)
+    gender = models.CharField(max_length=20)
+    age = models.IntegerField()
+    role = models.CharField(max_length=25)
+
---
+++ mwn/main/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import UserManagement
+# Create your views here.
+
+def mainpage(req):
+    return render(req,'index.html')
+
+def adddata(req):
+    if req.method == 'POST':
+        firstName = req.POST['firstName']
+        lastName = req.POST['lastName']
+        emailId = req.POST['emailId']
+        gender = req.POST['gender']
+        age = req.POST['age']
+        role = req.POST['role']
+        un = UserManagement(firstName=firstName,lastName=lastName,emailId=emailId,gender=gender,age=age,role=role)
+        un.save()
+        return render(req, 'index.html', {'message': 'Data Added'})
+    else:
+        return render(req, 'index.html', {'message': 'Data not Added'})
